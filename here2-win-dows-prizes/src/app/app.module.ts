8155946caf400e84c70d68384042d23f046ddb79
naveen <mnaveenkumar2009@gmail.com>
2018-03-18 16:45:05 +0530

    Authentication added

--- here2-win-dows-prizes/src/app/app.module.ts
+++ here2-win-dows-prizes/src/app/app.module.ts
@@ -2,20 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
-
-
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormsModule } from '@angular/forms';
+import {AuthService} from './auth.service';
+import { AngularFireAuth } from 'angularfire2/auth';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AuthService,AngularFireAuth],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
